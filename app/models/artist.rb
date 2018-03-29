30f142c3e5523b04ac6e8e4d402fa2ce618cfbf8
github username <github email address>
2018-03-29 13:22:22 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,15 +2,28 @@ class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
 
+  def get_first_song
+    self.songs.first 
+  end
+
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+    self.songs.first.genre 
   end
 
   def song_count
     #return the number of songs associated with the artist
+    self.songs.select do |song|
+      song.artist == self
+    end.size
   end
 
   def genre_count
     #return the number of genres associated with the artist
+    self.songs.select do |song|
+      if song.artist == self
+        song.artist.genres 
+      end
+    end.size
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,11 +1,20 @@
+require 'pry'
+
 class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
 
   def get_genre_name
+  	self.genre.name 
   end
 
   def drake_made_this
     # when this method is called it should assign the song's artist to Drake
+   	drake = Artist.all.map do |artist|
+   		if artist == drake
+   		 self.artist = artist
+   		end
+   	end.flatten[0]
+   	self.artist
   end
 end
